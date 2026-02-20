339d7bb299f9598bf6bae7e2f7115f830317b74b
Costin <Costin@OFFICE>
2026-02-19 19:46:36 -0500

    Add project files.

---
+++ src/WiFi_Watchdog.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		WiFi_Watchdog.cpp
+ Created:	2/19/2026 7:44:20 PM
+ Author:	Costin
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "WiFi_Watchdog.h"
+
+
---
+++ src/WiFi_Watchdog.h
@@ -0,0 +1,19 @@
+/*
+ Name:		WiFi_Watchdog.h
+ Created:	2/19/2026 7:44:20 PM
+ Author:	Costin
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _WiFi_Watchdog_h
+#define _WiFi_Watchdog_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
